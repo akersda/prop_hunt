acada5984946f8e5fd477db201af8c919b9b4b45
akersda <63028728+akersda@users.noreply.github.com>
2020-07-05 18:53:35 +0930

    Update scoreboard.lua

--- gamemode/ph_gui/scoreboard.lua
+++ gamemode/ph_gui/scoreboard.lua
@@ -6,6 +6,7 @@ function GM:ScoreboardShow()
 	scoreb:SetSize(400,200)
 	scoreb:Center()
 	function scoreb:Paint(w,h)
+		draw.RoundedBox( 4, 0, 0, w, w, Color(10,10,12,50) )
 		draw.Text({
 			text = gmtab.States[GetGlobalInt( "RoundState", 1 )] .. "  " .. GetGlobalInt( "RoundNum", 0 ) .. "   " .. math.Round(GetGlobalFloat( "RoundTime", 0.0 ),1),
 			pos = {4,4},
